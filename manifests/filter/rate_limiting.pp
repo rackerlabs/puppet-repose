d5a779751e9daf5fb44c882c479f6466dec7818c
Greg Swift <gregswift@gmail.com>
2013-04-26 15:46:18 -0500

    datastore is not required, also updated docs

--- manifests/filter/rate_limiting.pp
+++ manifests/filter/rate_limiting.pp
@@ -1,15 +1,15 @@
 # [*datastore*]
-# Whether or not to use a distributed datastore for storing maintaining
-# the rate limit information
+# String. Which distributed datastore to use for storing maintaining
+# the rate limit information.
 #
 # [*delegation*]
 # Boolean.  Dunno
 #
 # [*request_endpoint*]
-# Hash.  Contains String(uri-regex) and Boolean(include-absolute-limits)
+# Required Hash.  Contains String(uri-regex) and Boolean(include-absolute-limits)
 #
 # [*limit_groups*]
-# Array of hashes.  Hashes should contain
+# Required Array of hashes.  Hashes should contain
 #   String(id), String(groups), Boolean(default), and ArrayofHashes(limits)
 # Where the hashes in limits should contain the Strings:
 #   uri, uri-regex, http-methods, unit, value
@@ -35,11 +35,6 @@ class repose::filter::rate_limiting (
   }
   if $::debug {
     debug("\$ensure = '${ensure}'")
-  }
-
-## datastore
-  if $datastore == undef {
-    fail('datastore is a required parameter')
   }
 
 ## request_endpoint
