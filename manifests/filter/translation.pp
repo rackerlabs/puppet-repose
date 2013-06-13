1e050988000e5538f1dc785fe3f727793cc69c7c
Nick Bales <nick.bales@rackspace.com>
2013-06-13 10:05:15 -0500

    Adding Translation filter support

---
+++ manifests/filter/translation.pp
@@ -0,0 +1,45 @@
+# [*app_name*]
+#
+# [*request_translations*]
+# [*response_translations*]
+# List containing content_type, accept, translated_content_type, and a list of styles
+#
+class repose::filter::versioning (
+  $ensure     = present,
+  $app_name   = 'repose',
+  $request_translation= undef,
+  $response_translation= undef,
+) inherits repose::params {
+
+### Validate parameters
+
+## ensure
+  if ! ($ensure in [ present, absent ]) {
+    fail("\"${ensure}\" is not a valid ensure parameter value")
+  } else {
+    $file_ensure = $ensure ? {
+      present => file,
+      absent  => absent,
+    }
+  }
+  if $::debug {
+    debug("\$ensure = '${ensure}'")
+  }
+
+## request_translation, response_translation
+  if $request_translation == undef || $response_translation == undef {
+    fail('either request_translations or response_translations are required')
+  }
+
+## Manage actions
+
+  file { "${repose::params::configdir}/tranlations.cfg.xml":
+    ensure  => file,
+    owner   => $repose::params::owner,
+    group   => $repose::params::group,
+    mode    => $repose::params::mode,
+    require => Package['repose-filters'],
+    content => template('repose/translations.cfg.xml.erb'),
+  }
+
+}
