b5225077255f2fc542e54387631f15b1e02c619d
Tony Rogers <tony.rogers@rackspace.com>
2017-01-24 16:22:38 -0500

    add default fact for $debug because of strict variables in puppet4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-utils'
+
+RSpec.configure do |c|
+  c.default_facts = {
+    :debug                     => 'true',
+  }
+end
