f8c41ac6553f87d35469cb1e90e6934d0ec26341
Adrian George <adrian.george@rackspace.com>
2017-07-31 08:10:13 -0500

    Added the keystone v2 basic auth filter

--- manifests/filter/keystone_v2_basic_auth.pp
+++ manifests/filter/keystone_v2_basic_auth.pp
@@ -1,8 +1,7 @@
-# == Resource: repose::filter::rackspace_identity_basic_auth
+# == Resource: repose::filter::keystone_v2_basic_auth
 #
-# This is a resource for generating rackspace-identity-basic-auth configuration
-# files
-# DEPRECATED: In repose 8+ use keystone_v2_basic_auth instead
+# This is a resource for generating keystone-v2-basic-auth configuration
+# files. This supersedes rackspace_identity_basic_auth as of Repose 8.0.0.0.
 #
 # === Parameters
 #
@@ -12,20 +11,31 @@
 #
 # [*filename*]
 # String. Filename to output config
-# Defaults to <tt>rackspace-identity-basic-auth.cfg.xml</tt>
+# Defaults to <tt>keystone-v2-basic-auth.cfg.xml</tt>
 #
 # [*identity_service_url*]
 # String. Url to the identity endpoint to use for this filter.
-# Defaults to <tt>https://identity.api.rackspacecloud.com/v2.0/tokens</tt>
+# Defaults to <tt>undef</tt>
 #
 # [*token_cache_timeout*]
 # Integer. Time to cache the token in milliseconds
-# Defaults to <tt>600000</tt>
+# Defaults to <tt>undef</tt> (repose default is 600000)
+#
+# [*connection_pool_id*]
+# String. The name of a pool from http-connection-pool.cfg.xml. Setting this
+# tells the connection pool service to map to the pool with specified id. If
+# default is chosen, the default connection pool configurations in connection
+# pool service is used.
+# Defaults to <tt>undef</tt>
+#
+# [*secret_type*]
+# String. Determines how authentication should be attempted. Valid values are api-key and password.
+# Defaults to <tt>undef</tt> (repose defaults to api-key)
 #
 # [*delegating*]
 # Enable delegating mode to allow the herp/derp filters to publish
 # rejected requests to flume.
-# Defaults to <tt>false</tt>
+# Defaults to <tt>undef</tt> (repose default is false)
 #
 # [*delegating_quality*]
 # Set the quality for this filter when returning error responses.
@@ -33,25 +43,28 @@
 #
 # === Links
 #
+# * http://www.openrepose.org/versions/latest/filters/keystone-v2-basic-auth.html
 # * https://repose.atlassian.net/wiki/display/REPOSE/Rackspace+Identity+Basic+Authentication+filter
 #
 # === Examples
 #
-# repose::filter::rackspace_identity_basic_auth {
+# repose::filter::keystone_v2_basic_auth {
 #   'default':
 # }
 #
 # === Authors
 #
-# * Alex Schultz <mailto:alex.schultz@rackspace.com>
+# * Adrian George <mailto:adrian.george@rackspace.com>
 # * c/o Cloud Integration Ops <mailto:cit-ops@rackspace.com>
 #
-define repose::filter::rackspace_identity_basic_auth (
+define repose::filter::keystone_v2_basic_auth (
   $ensure               = present,
-  $filename             = 'rackspace-identity-basic-auth.cfg.xml',
-  $identity_service_url = 'https://identity.api.rackspacecloud.com/v2.0/tokens',
-  $token_cache_timeout  = '600000',
-  $delegating           = false,
+  $filename             = 'keystone-v2-basic-auth.cfg.xml',
+  $identity_service_url = undef,
+  $token_cache_timeout  = undef,
+  $connection_pool_id   = undef,
+  $secret_type          = undef,
+  $delegating           = undef,
   $delegating_quality   = undef,
 ) {
 
@@ -75,7 +88,12 @@ define repose::filter::rackspace_identity_basic_auth (
     if $identity_service_url == undef {
       fail('identity_service_url is a required parameter')
     }
-    $content_template = template("${module_name}/rackspace-identity-basic-auth.cfg.xml.erb")
+
+    if ($secret_type != undef) and (! ($secret_type in [api-key, password])) {
+      fail('secret_type must have a value of api-key or password')
+    }
+
+    $content_template = template("${module_name}/keystone-v2-basic-auth.cfg.xml.erb")
   } else {
     $content_template = undef
   }
