fd958c1b1f7be696d01df47308eebbf186a38ff3
Joel Rizner <joel.rizner@rackspace.com>
2017-12-20 11:10:15 -0600

    Templated values for header-user filter

--- spec/defines/filter/header_user.rb
+++ spec/defines/filter/header_user.rb
@@ -34,25 +34,6 @@ describe 'repose::filter::header_user', :type => :define do
       }
     end
 
-    context 'providing header user definitions' do
-      let(:title) { 'header_user' }
-      let(:params) { {
-          :ensure     => 'present',
-          :filename   => 'header-user.cfg.xml',
-      } }
-      it {
-        should contain_file('/etc/repose/header-user.cfg.xml').with_content(
-                   /<header id=\\"X-Auth-Token\\" quality=\\".95\\"\/>/
-               ).with_content(
-                   /<header id=\\"X-Forwarded-For\\" quality=\\"0.5\\"\/>/
-               ).with_content(
-                   /<source-headers>/
-               ).with_content(
-                   /<\/source-headers>/
-               )
-      }
-    end
-
     context 'with defaults with new namespace' do
       let :pre_condition do
         "class { 'repose': cfg_new_namespace => true }"
