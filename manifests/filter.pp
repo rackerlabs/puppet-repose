3ef5d3831ec1c13966562ab57c341ff9b5af192e
Cory Ringdahl <13054045+cringdahl@users.noreply.github.com>
2021-06-02 22:10:16 -0500

    repose::filter restructure for hiera

--- manifests/filter.pp
+++ manifests/filter.pp
@@ -15,4 +15,21 @@
 # * Greg Swift <mailto:greg.swift@rackspace.com>
 # * c/o Cloud Integration Ops <mailto:cit-ops@rackspace.com>
 #
-class repose::filter { fail('This class should not be used directly') }
+class repose::filter {
+  # merging same hash from multiple sources, innermost hash defs first
+  $filters = lookup('repose::filter', { 'merge' => 'deep' })
+
+  # these are classes so we'll instantiate classes
+  $classes = ['atom_feed_service','container','http_connection_pool']
+  $filters.each |$k,$v| {
+    if $k in $classes {
+      class { "repose::filter::${k}":
+        * => $v
+      }
+    } else {
+  # all the rest of the filters are defined_types with nested hashes, 
+  # so we can run create_resources    
+      create_resources("repose::filter::${k}", $v)
+    }
+  }
+}
